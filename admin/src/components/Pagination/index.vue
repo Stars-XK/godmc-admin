a390d6d818edf5991182d714d04178d6e0feeb49
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-19 16:09:15 +0000

    style: [UI/UX] 修复分页组件大白块背景及表格分离的样式问题，为 app-container 统一添加卡片效果

--- admin/src/components/Pagination/index.vue
+++ admin/src/components/Pagination/index.vue
@@ -96,8 +96,7 @@ function handleCurrentChange(val) {
 
 <style scoped>
 .pagination-container {
-  background: #fff;
-  padding: 32px 16px;
+  background: transparent;
 }
 .pagination-container.hidden {
   display: none;
