1af9f0ed5c389eda678bde296fcfdcf0103964f5
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-19 15:36:48 +0000

    feat: 微服务改造计划执行
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- admin/src/layout/components/AppMain.vue
+++ admin/src/layout/components/AppMain.vue
@@ -21,24 +21,16 @@ const tagsViewStore = useTagsViewStore()
 <style lang="scss" scoped>
 .app-main {
   /* 50= navbar  50  */
-  min-height: calc(100vh - 50px);
+  min-height: calc(100vh - 60px);
   width: 100%;
   position: relative;
   overflow: hidden;
 }
 
-.fixed-header + .app-main {
-  padding-top: 50px;
-}
-
 .hasTagsView {
   .app-main {
     /* 84 = navbar + tags-view = 50 + 34 */
-    min-height: calc(100vh - 84px);
-  }
-
-  .fixed-header + .app-main {
-    padding-top: 84px;
+    min-height: calc(100vh - 94px);
   }
 }
 </style>
