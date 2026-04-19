ce1ee90cac5616c21a804354cfc0447ba469713e
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-19 12:47:57 +0000

    fix: [修复] 修改配置文件环境变量映射错误，并更新dev.yml中提供的默认配置

--- server/libs/shared/src/config/index.ts
+++ server/libs/shared/src/config/index.ts
@@ -18,12 +18,14 @@ export default () => {
   if (process.env.DB_TYPE) config.db.mysql.type = process.env.DB_TYPE;
   if (process.env.DB_HOST) config.db.mysql.host = process.env.DB_HOST;
   if (process.env.DB_PORT) config.db.mysql.port = parseInt(process.env.DB_PORT, 10);
-  if (process.env.DB_USERNAME) config.db.mysql.username = process.env.DB_USERNAME;
-  if (process.env.DB_PASSWORD) config.db.mysql.password = process.env.DB_PASSWORD;
-  if (process.env.DB_DATABASE) config.db.mysql.database = process.env.DB_DATABASE;
+  if (process.env.DB_USER) config.db.mysql.username = process.env.DB_USER;
+  if (process.env.DB_PWD) config.db.mysql.password = process.env.DB_PWD;
+  if (process.env.DB_NAME) config.db.mysql.database = process.env.DB_NAME;
   
   if (process.env.REDIS_HOST) config.redis.host = process.env.REDIS_HOST;
-  if (process.env.REDIS_PASSWORD) config.redis.password = process.env.REDIS_PASSWORD;
+  if (process.env.REDIS_PORT) config.redis.port = parseInt(process.env.REDIS_PORT, 10);
+  if (process.env.REDIS_PWD) config.redis.password = process.env.REDIS_PWD;
+  if (process.env.REDIS_DB) config.redis.db = parseInt(process.env.REDIS_DB, 10);
   
   if (process.env.MICRO_AUTH_HOST) config.microservices.auth.host = process.env.MICRO_AUTH_HOST;
   if (process.env.MICRO_AUTH_PORT) config.microservices.auth.port = parseInt(process.env.MICRO_AUTH_PORT, 10);
