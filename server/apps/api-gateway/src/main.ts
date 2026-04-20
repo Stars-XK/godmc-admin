40cba64650051e23776833c7c08e7021455c2b41
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-20 16:24:25 +0000

    fix: [网关] 将 auth 相关的根路由代理目标改回 micro-system 解决 404 问题

--- server/apps/api-gateway/src/main.ts
+++ server/apps/api-gateway/src/main.ts
@@ -91,11 +91,11 @@ async function bootstrap() {
   const proxies = [
     // 将不需要加模块前缀的根级接口直接打给 micro-system 处理
     { path: '/login', target: 'http://127.0.0.1:3002' },
-    { path: '/logout', target: 'http://127.0.0.1:3001' },
-    { path: '/register', target: 'http://127.0.0.1:3001' },
+    { path: '/logout', target: 'http://127.0.0.1:3002' },
+    { path: '/register', target: 'http://127.0.0.1:3002' },
     { path: '/getInfo', target: 'http://127.0.0.1:3002' },
     { path: '/getRouters', target: 'http://127.0.0.1:3002' },
-    { path: '/captchaImage', target: 'http://127.0.0.1:3001' },
+    { path: '/captchaImage', target: 'http://127.0.0.1:3002' },
 
     { path: '/system', target: 'http://127.0.0.1:3002' },
     { path: '/auth', target: 'http://127.0.0.1:3001' },
