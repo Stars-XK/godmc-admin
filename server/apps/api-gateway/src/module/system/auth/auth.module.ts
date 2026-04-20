1e7a89c8082cbce9dd0bdd6243b538ee03c3ebef
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-20 10:06:01 +0000

    fix: [修复] api-gateway缺失JwtStrategy导致的 Unknown authentication strategy jwt 错误

--- server/apps/api-gateway/src/module/system/auth/auth.module.ts
+++ server/apps/api-gateway/src/module/system/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
+  imports: [PassportModule],
   controllers: [AuthController],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
