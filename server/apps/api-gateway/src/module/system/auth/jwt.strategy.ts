b1d4b00c7e20f56189b28422c5220459c3d53ef7
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-20 10:15:26 +0000

    fix: [修复] 网关鉴权丢失用户信息导致/getInfo报错及前端强制退出问题

--- server/apps/api-gateway/src/module/system/auth/jwt.strategy.ts
+++ server/apps/api-gateway/src/module/system/auth/jwt.strategy.ts
@@ -1,11 +1,16 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { RedisService } from '@app/shared';
+import { CacheEnum } from '@app/common/enum/index';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly redisService: RedisService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,7 +18,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return { ...payload };
+  async validate(payload: { uuid: string; userId: string; iat: Date }) {
+    const user = await this.redisService.get(`${CacheEnum.LOGIN_TOKEN_KEY}${payload.uuid}`);
+    if (!user) throw new UnauthorizedException('登录已过期，请重新登录');
+    return user;
   }
 }
