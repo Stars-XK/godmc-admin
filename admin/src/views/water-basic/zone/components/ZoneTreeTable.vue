da813af20c05c8b19fcb879ae8b124e5f2437cd1
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-21 03:00:20 +0000

    style: [样式] 优化分区树形表格宽度，名称列自适应并显示 tooltip，操作列固定宽度

--- admin/src/views/water-basic/zone/components/ZoneTreeTable.vue
+++ admin/src/views/water-basic/zone/components/ZoneTreeTable.vue
@@ -6,7 +6,7 @@
     :default-expand-all="isExpandAll"
     :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
   >
-    <el-table-column prop="name" label="分区名称" width="260"></el-table-column>
+    <el-table-column prop="name" label="分区名称" min-width="260" show-overflow-tooltip></el-table-column>
     <el-table-column prop="code" label="分区编码" width="150"></el-table-column>
     <el-table-column prop="area" label="面积(k㎡)" width="120"></el-table-column>
     <el-table-column prop="population" label="服务人口" width="120"></el-table-column>
@@ -21,7 +21,7 @@
         <span>{{ parseTime(scope.row.createTime) }}</span>
       </template>
     </el-table-column>
-    <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+    <el-table-column label="操作" align="center" width="220" class-name="small-padding fixed-width">
       <template #default="scope">
         <el-button link type="primary" icon="Edit" @click="$emit('edit', scope.row)" v-hasPermi="['water-basic:zone:edit']">修改</el-button>
         <el-button link type="primary" icon="Plus" @click="$emit('add', scope.row)" v-hasPermi="['water-basic:zone:add']">新增</el-button>
