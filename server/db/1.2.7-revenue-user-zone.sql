d2fafb8c86d59169488b882128da9c5b4fa5e501
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-21 10:21:22 +0000

    fix: [修复] 修改数据库更新脚本为幂等执行，防止每次重启服务时报 Duplicate column name 错误

--- server/db/1.2.7-revenue-user-zone.sql
+++ server/db/1.2.7-revenue-user-zone.sql
@@ -1,2 +1,12 @@
-ALTER TABLE `scada_revenue_user`
-ADD COLUMN `zone_code` varchar(50) DEFAULT NULL COMMENT '所属分区编码' AFTER `user_type`;
+DELIMITER $$
+CREATE PROCEDURE AddColumnsIfNotExist_1_2_7()
+BEGIN
+    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'scada_revenue_user' AND COLUMN_NAME = 'zone_code') THEN
+        ALTER TABLE `scada_revenue_user`
+        ADD COLUMN `zone_code` varchar(50) DEFAULT NULL COMMENT '所属分区编码' AFTER `user_type`;
+    END IF;
+END $$
+DELIMITER ;
+
+CALL AddColumnsIfNotExist_1_2_7();
+DROP PROCEDURE AddColumnsIfNotExist_1_2_7;
