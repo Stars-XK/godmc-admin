8e0659f52dbb9382ae8cc375143aa690378f6bd9
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-22 04:23:39 +0000

    fix: [监控] 修复 StatusEngineModule 缺少 TCP Client 依赖注入的问题

--- server/apps/micro-monitor/src/module/monitor/status-engine/status-engine.module.ts
+++ server/apps/micro-monitor/src/module/monitor/status-engine/status-engine.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 import { WaterPointEntity, WaterDeviceEntity, WaterStationEntity } from '@app/common';
 import { StatusEngineService } from './status-engine.service';
+import { MicroservicesModule } from '../../../microservices.module';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     TypeOrmModule.forFeature([WaterPointEntity, WaterDeviceEntity, WaterStationEntity]),
+    MicroservicesModule,
   ],
   providers: [StatusEngineService],
 })
