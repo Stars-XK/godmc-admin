17a645b8b4eb3371085c72386f6ac1d834a65d0b
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-22 04:22:11 +0000

    fix: [监控] 修复 TCP client 依赖注入与微服务全局模块冲突问题

--- server/apps/micro-monitor/src/microservices.module.ts
+++ server/apps/micro-monitor/src/microservices.module.ts
@@ -10,6 +10,11 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         transport: Transport.TCP,
         options: { host: process.env.MICRO_SYSTEM_HOST || '127.0.0.1', port: 3002 },
       },
+      {
+        name: 'MICRO_WATER_BASIC',
+        transport: Transport.TCP,
+        options: { host: process.env.MICRO_WATER_BASIC_HOST || '127.0.0.1', port: 3006 },
+      },
     ]),
   ],
   exports: [ClientsModule],
