4672516bf840edc0c48e565e136a0547b858f1a2
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-28 13:08:30 +0000

    chore: 清理 server 临时脚本

--- server/query_test.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-async function run() {
-  try {
-    const res = await axios.get('http://127.0.0.1:3000/water-basic/station/list', { headers: { 'Authorization': 'Bearer admin' } });
-    console.log(Object.keys(res.data));
-    console.log(res.data.data ? Object.keys(res.data.data) : 'No data');
-  } catch(e) { console.log(e.message); }
-}
-run();
