e8071c13df28faf80a915e506c37ddcc41f4b024
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-28 07:55:37 +0000

    feat: 修复登录功能报错500
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- test-proxy2.js
+++
@@ -1,6 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const proxy = createProxyMiddleware({ target: 'http://localhost:3000', pathFilter: '/system' });
-const req1 = { url: '/system/user', method: 'GET' };
-const req2 = { url: '/sys', method: 'GET' };
-console.log('Matches /system/user?', proxy.pathFilter(req1.url, req1));
-console.log('Matches /sys?', proxy.pathFilter(req2.url, req2));
